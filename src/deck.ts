d1f9f10d2646e66b46450bc321b291985a4aa6e9
Kai Giokas <kai@kaictl.me>
2020-10-23 16:48:51 -0600

    Better trump card drawing, check for enough cards

--- src/deck.ts
+++ src/deck.ts
@@ -10,6 +10,8 @@ interface Card {
     suit: Suit
 }
 
+type MaybeCard = Card | null
+
 function cardCompare(card1: Card, card2: Card): ordering {
     return card1.rank - card2.rank
 }
@@ -35,9 +37,14 @@ class Deck {
         // * add all of the cards from the deck
         // * give each player 6 random cards
         // * pull random remaining card for trump
+        let totalDealtCards: number = cardsPerPlayer * players
+        let totalCards: number = 4 * (13 - (startingCard - 1))
+        if (totalCards < (totalDealtCards + 1)) {
+            throw new Error("Not enough cards: decrease players, increase total cards, or lower cards per player.")
+        }
         this.deck = this.createDeck(startingCard)
         this.hands = this.deal(players, cardsPerPlayer)
-        this.trumpCard = this.finalDraw()
+        this.trumpCard = this.draw() as Card  // this should never be undefined, but...
         this.trump = this.trumpCard.suit
         this.drawBelow = cardsPerPlayer
         this.finalTrumpLeft = true
@@ -60,35 +67,29 @@ class Deck {
         return Math.floor(Math.random() * end)
     }
 
-    draw(): Card | undefined {
+    draw(): MaybeCard {
         if (this.deck.length > 0) {
             return this.deck.splice(this.random(this.deck.length), 1)[0]
         } else if (this.finalTrumpLeft) {
             this.finalTrumpLeft = false
             return this.trumpCard
-        } else return undefined
+        } else return null
     }
 
     deal(players: number, cardsPerPlayer: number): Card[][] {
         let cards: Card[][] = []
-        let drawncard: Card | undefined
+        let drawncard: MaybeCard
         for(let c = 0; c < cardsPerPlayer; c++) {
             for (let p = 0; p < players; p++) {
                 cards.push([])
                 drawncard = this.draw()
                 if (drawncard) {
-                    cards[p].push(drawncard)
+                    cards[p].push(drawncard as Card)
                 }
             }
         }
         this.hands = cards
         return cards
-    }
-
-    finalDraw(): Card {
-        let c: Card = this.draw()
-        this.trumpCard = c
-        return c
     }
 }
 
@@ -97,3 +98,6 @@ console.log(newdeck.deck)
 console.log(newdeck.hands)
 console.log(newdeck.trumpCard)
 console.log(`Total Cards: ${newdeck.deck.length + newdeck.hands.length * 6 + 1}`)
+
+console.log(cardCompare({rank: 4, suit: "spades"}, {rank: 5, suit: "spades"}))
+console.log(cardCompareTrump({rank: 4, suit: "hearts"}, {rank: 5, suit: "spades"}, "hearts"))

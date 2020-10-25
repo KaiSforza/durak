db8848c4e037d23f1cc39e365abac1bad8a13d5e
Kai Giokas <kai@kaictl.me>
2020-10-25 13:07:25 -0600

    Add testing, move around functions, privates

---
+++ src/deck.test.ts
@@ -0,0 +1,31 @@
+import * as deck from "./deck"
+
+    // let d = new deck.Deck(2, 6, 6)
+test('compares cards', () => {
+    let c1: deck.Card = {rank: 2, suit: "clubs"}
+    let c2: deck.Card = {rank: 6, suit: "clubs"}
+    expect(deck.cardCompare(c1, c2)).toBeLessThan(0)
+})
+
+test('compare cards with trump', () => {
+    let c1: deck.Card = {rank: 2, suit: "hearts"}
+    let c2: deck.Card = {rank: 6, suit: "clubs"}
+    let c3: deck.Card = {rank: 3, suit: "hearts"}
+    expect(deck.cardCompareTrump(c1, c2, "hearts")).toBeGreaterThan(0)
+    expect(deck.cardCompareTrump(c1, c3, "hearts")).toBeLessThan(0)
+})
+
+test('card count', () => {
+    let d1 = new deck.Deck(4, 6, 6)
+    expect(d1.deck.length).toEqual(11)
+    expect(d1.hands.length).toEqual(24)
+    expect(d1.hands[0].length).toEqual(6)
+})
+
+test('not enough cards', () => {
+    try {
+        let d2 = new deck.Deck(10, 10, 10)
+    } catch(e) {
+        expect(e.__proto__.name).toEqual("Error")
+    }
+})

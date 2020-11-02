e1319be02d7d626fc2564ce68493abd8a1bc1895
Kai Giokas <kai@kaictl.me>
2020-11-02 10:38:21 -0700

    Import all types to common.test

--- test/common.test.ts
+++ test/common.test.ts
@@ -1,17 +1,17 @@
-import type { Card } from '../src/types'
+import type * as t from '../src/types'
 import { cardCompare, cardCompareTrump } from '../src/common'
 
     // let d = new deck.Deck(2, 6, 6)
 test('compares cards', () => {
-    let c1: Card = {rank: 2, suit: "clubs"}
-    let c2: Card = {rank: 6, suit: "clubs"}
+    let c1: t.Card = {rank: 2, suit: "clubs"}
+    let c2: t.Card = {rank: 6, suit: "clubs"}
     expect(cardCompare(c1, c2)).toBeLessThan(0)
 })
 
 test('compare cards with trump', () => {
-    let c1: Card = {rank: 2, suit: "hearts"}
-    let c2: Card = {rank: 6, suit: "clubs"}
-    let c3: Card = {rank: 3, suit: "hearts"}
+    let c1: t.Card = {rank: 2, suit: "hearts"}
+    let c2: t.Card = {rank: 6, suit: "clubs"}
+    let c3: t.Card = {rank: 3, suit: "hearts"}
     expect(cardCompareTrump(c1, c2, "hearts")).toBeGreaterThan(0)
     expect(cardCompareTrump(c2, c1, "hearts")).toBeLessThan(0)
     expect(cardCompareTrump(c1, c3, "hearts")).toBeLessThan(0)
